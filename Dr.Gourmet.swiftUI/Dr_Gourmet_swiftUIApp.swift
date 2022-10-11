2edb19753a3a96efca52b4d37a309bdafed651a9
someng <bsm4045@naver.com>
2022-10-12 00:58:11 +0900

    Initial Commit

---
+++ Dr.Gourmet.swiftUI/Dr_Gourmet_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dr_Gourmet_swiftUIApp.swift
+//  Dr.Gourmet.swiftUI
+//
+//  Created by 백소망 on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct Dr_Gourmet_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dr.Gourmet.swiftUIUITests/Dr_Gourmet_swiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Dr_Gourmet_swiftUIUITestsLaunchTests.swift
+//  Dr.Gourmet.swiftUIUITests
+//
+//  Created by 백소망 on 2022/10/12.
+//
+
+import XCTest
+
+final class Dr_Gourmet_swiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
